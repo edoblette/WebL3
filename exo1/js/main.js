232078d7e9ec84853d5aba7a77999785309472f3
eddy <edwardoblette@gmail.com>
2019-10-09 00:52:25 +0200

    Color [ok]

--- exo1/js/main.js
+++ exo1/js/main.js
@@ -7,17 +7,37 @@ window.addEventListener("load", event => {
 
 class  element {
 
-    constructor(container, color, id) {
-	    this.element = document.createElement("div");
-	    this.element.id = id;
-	    this.element.className = "element";
-		this.element.style.height = "50px";
-		this.element.style.width = "50px";
-		this.element.style.backgroundColor = color;
-		this.element.style.position = "absolute";
-		this.element.style.left = "10px";
-		this.element.style.top =  (50 * id) + "px";
-		container.appendChild(this.element);
+    constructor(container, id, color,  name) {
+	    let newDiv = document.createElement("div");
+	    newDiv.id = id;
+	    newDiv.className = "element";
+		newDiv.style.height = "50px";
+		newDiv.style.width = "300px";
+		newDiv.style.position = "absolute";
+		newDiv.style.left = "10px";
+		newDiv.style.top =  (50 * id) + 10 + "px";
+		newDiv.style.margin = "10px";
+
+
+		
+
+		let newDivContentImg = document.createElement("img");
+		newDivContentImg.id = id;
+		newDivContentImg.className = "color";
+		newDivContentImg.style.height = "50px";
+		newDivContentImg.style.width = "50px";
+		newDivContentImg.style.backgroundColor = color;
+
+		let newDivContent = document.createElement("a");
+		newDivContent.innerHTML = name;
+		newDivContent.style.position = "relative";
+		newDivContent.style.top =  "-20px";
+		newDivContent.style.left =  "20px";
+		newDivContent.className = "label";		
+
+		newDiv.appendChild(newDivContentImg);
+		newDiv.appendChild(newDivContent);
+		container.appendChild(newDiv);
     }
 
     getColor() {
@@ -33,8 +53,9 @@ const main = event => {
 	//init var
 	let front_element = 0;
 	let isDragging = false;
-	let screen_size = 50;
-	let color = ["255,0,0", "255,0,0", "255,0,0", "255,0,0", "255,0,0", "255,0,0", "255,0,0", "255,0,0", "255,0,0", "255,0,0", "255,0,0"];
+	let screen_size = 60;
+	let color_label = ["Klein Blue", "Vibrant Yellow", "Living Coral", "Chive Blossom", "Vivacious", "Barrier Reef", "Deep Lake", "Ibiza Blue", "Pink Lemonade", "Blue Depths"];
+	let color       = ["0, 47, 167", "255,218,41", "255,111,97", "125,93,153", "163,40,87", "0,132,161", "0,101,107", "	0,124,183", "238,109,138", "38,48,86"];
 	console.log("MAIN");
 
 	let container = document.createElement("div");
@@ -44,7 +65,8 @@ const main = event => {
 	for(var i = 0; i < 10; i++) {
 		let color_pick = ("rgba(" + color[i] + ',1)');
 		console.log(color_pick)
-		var square = new element(container,color_pick, i);
+		var square = new element(container,i, color_pick, color_label[i]);
+
 		//square.setColor("red")
 	}
 		
@@ -73,22 +95,20 @@ const main = event => {
 			if((distance >= 0) && (distance < 10))
 				front_element = distance			
 
-			
 
 			console.log(distance );
 			for(let i = 0; i < 10; i++ ){
-				let distanceFront = Math.abs(front_element - i);
-				let formula = (-2 * Math.pow(distanceFront,2)) + 100 ;
+				let distanceFront = i - front_element;
+				let formula = (-2 * Math.pow( Math.abs(distanceFront),2)) + 100 ;
 				console.log("je suis a une distance" + ' ' + distanceFront + ' ' + (formula/10));
+				if(i == front_element)
+					formula += 25;
 				if(formula > 0){
 						tab[i].style.left =  (formula) + "px";
-						tab[i].style.top =  (50 * ((front_element - i) + 6 )) + "px";
-						
-				}else{
-						tab[i].style.left = "0px";
-						tab[i].style.backgroundColor = "rgb(255,0,0)";	
+						tab[i].style.top =  (50 * (distanceFront + 6 )) + "px";	
 				}
-				tab[i].style.backgroundColor = "rgb(0,0,0, "+ ( ((-3 * Math.pow(distanceFront,2)) + 100)/120) + ")";
+				tab[i].style.opacity = (((-5 * Math.pow(distanceFront,2)) + 100)/100) ;
+				tab[i].style.transitionDuration = "1s";
 			}
 
 			
